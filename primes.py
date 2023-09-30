b1007a4081c34095d01654477b3e02f103db28fb
sauceboss44 <alexrudakov5@gmail.com>
2023-09-30 16:58:21 +0100

    Add solution

--- primes.py
+++ primes.py
@@ -2,5 +2,20 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
-    list = []
+    if number_of_primes <= 0:
+        raise ValueError
+    list = [2]
+    number = 3
+    while len(list) < number_of_primes:
+        print(number)
+        if isPrime(number):
+            list.append(number)
+        number += 1
     return list
+def isPrime(number):
+    for divisor in range(2,int(number**1/2)+1):
+        if number % divisor == 0:
+            return False
+    return True
+
+print(primes(10))
